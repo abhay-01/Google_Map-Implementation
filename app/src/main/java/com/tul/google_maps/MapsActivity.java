4d2d223112e5f1367713938c154bf80f570e3723
abhay-01 <pratapsinghabhay999@gmail.com>
2023-05-18 15:23:43 +0530

    Initial Commit

---
+++ app/src/main/java/com/tul/google_maps/MapsActivity.java
@@ -0,0 +1,45 @@
+package com.tul.google_maps;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.tul.google_maps.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        mMap = googleMap;
+
+        LatLng latLng = new LatLng(28.5355,77.3910);  //Noida Lat and Lang
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Noida");
+        mMap.addMarker(markerOptions);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,16f));
+    }
+}
