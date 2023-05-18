3ece4420fe87469d9e9854efe88827364354b2b1
abhay-01 <pratapsinghabhay999@gmail.com>
2023-05-18 16:05:02 +0530

    Add Overlay

--- app/src/main/java/com/tul/google_maps/MapsActivity.java
+++ app/src/main/java/com/tul/google_maps/MapsActivity.java
@@ -10,8 +10,10 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
 import com.tul.google_maps.databinding.ActivityMapsBinding;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback{
@@ -32,7 +34,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
+    public void onMapReady(GoogleMap googleMap) {
 
         mMap = googleMap;
 
@@ -41,5 +43,23 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap.addMarker(markerOptions);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,16f));
+
+        // Circle
+
+        mMap.addCircle(new CircleOptions()
+                .center(latLng)
+                .radius(1000)
+                .fillColor(R.color.purple_700)
+                .strokeColor(R.color.black));
+
+        //Polygon
+
+        mMap.addPolygon(new PolygonOptions()
+                .add(new LatLng(28.5355,77.3910),
+                        new LatLng(28.5355,79.3910),
+                        new LatLng(29.5355,79.3910),
+                        new LatLng(29.5355,77.3910),
+                        new LatLng(28.5355,77.3910)).fillColor(R.color.black));
+
     }
 }
